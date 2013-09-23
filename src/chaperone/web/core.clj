f58744eed90d9993337ae51e4773d5e720b3ca67
Mark Mandel <mark.mandel@gmail.com>
2013-09-23 16:20:56 +1000

    WIP on trying to get dieter to work.

--- src/chaperone/web/core.clj
+++ src/chaperone/web/core.clj
@@ -5,13 +5,21 @@
 			 [compojure.core :as comp]
 			 [compojure.handler :as handler]
 			 [compojure.route :as route]
-			 [selmer.parser :as selmer]))
+			 [selmer.parser :as selmer]
+			 [dieter.core :as dieter]))
 
 ;;; system tools
 (defn create-sub-system
 	"Create the persistence system. Takes the existing system details"
 	[system]
-	(let [sub-system {:port (env/env :web-server-port 8080)}]
+	(let [sub-system {:port   (env/env :web-server-port 8080)
+					  :dieter {:engine     :rhino ; defaults to :rhino; :v8 is much much faster
+							   :compress   false ; minify using Google Closure Compiler & Less compression
+												  ;:asset-roots ["resources/assets"] ; must have a folder called 'assets'. Searched for assets in the order listed.
+												  ;:cache-root  "resources/assets-cache" ; compiled assets are cached here
+							   :cache-mode :development ; or :production. :development disables cacheing
+							   :log-level  :normal} ; or :quiet
+					  }]
 		(assoc system :web sub-system)))
 
 (defn sub-system
@@ -28,8 +36,15 @@
 
 ;basic configuration
 (comp/defroutes site-routes
-				(comp/GET "/" [] (selmer/render-file "views/index.html" {}))
+				(comp/GET "/" [] (selmer/render-file "views/index.html"
+													 {:foo  "bar",
+													  :less (dieter/link-to-asset "main.less")}))
 				(route/not-found "<h1>404 OMG</h1>"))
+
+(defn run-server
+	"runs the server, and returns the stop function"
+	[web port]
+	(server/run-server (-> #'site-routes handler/site (dieter/asset-pipeline (:dieter web))) {:port port}))
 
 (defn start
 	"Start the web server, and get this ball rolling"
@@ -39,15 +54,14 @@
 		  port (:port web)]
 		(if-not (:server web)
 			(assoc system :web
-				  (assoc web :server (server/run-server (handler/site #'site-routes) {:port port})))
+						  (assoc web :server (run-server web port)))
 			system)))
 (defn stop
 	"Oh noes. Stop the server!"
 	[system]
 	(let [web (sub-system system)]
-		(println "stopping web: " web)
 		(if (:server web)
-					((:server web))))
+			((:server web))))
 	system)
 
 ;(run-server (site #'hello-world) {:port 8080})
