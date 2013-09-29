2ca7ce8a415e61e836c5ce5daaccaaa70509cbc8
Mark Mandel <mark.mandel@gmail.com>
2013-09-29 15:36:36 +1000

    Got cljs compiling, and got a small setup for Angular routing started.

--- project.clj
+++ project.clj
@@ -7,24 +7,26 @@
 						   [clj-time "0.6.0"]
 						   [cljs-uuid "0.0.4"]
 						   [environ "0.4.0"]
-						   [clojurewerkz/elastisch "1.3.0-beta1"]
+						   [clojurewerkz/elastisch "1.3.0-beta3"]
 						   [org.clojure/clojurescript "0.0-1889"]
 						   [purnam "0.1.0-beta"]
 						   [http-kit "2.1.10"]
 						   [compojure "1.1.5"]
 						   [selmer "0.4.2"]
-						   [dieter "0.4.1"]
-						   ]
+						   [dieter "0.4.1"]]
 			:main chaperone.core
 			:profiles {:uberjar {:aot :all}
 					   :dev     {:dependencies [[midje "1.6-beta1"]
-												[org.clojure/tools.namespace "0.2.4"]
-												[org.clojure/tools.trace "0.7.6"]]
+												[org.clojure/tools . namespace "0.2.4"]
+												[org.clojure/tools . trace "0.7.6"]]
 								 :source-paths ["dev"]
 								 :repl-options {:init-ns user}
-								 :env          {:elasticsearch-url  "http://dev.chaperone:9200"
+								 :env          {:elasticsearch-url   "http://dev.chaperone:9200"
 												:elasticsearch-index "test_chaperone"
-												:web-server-port 8080}}}
+												:web-server-port     8080}}
+					   ;; profile for compiling cljs, to remove unneccessary dependencies, and since
+					   ;; it will blow up with dieter as it looks for the v8 native
+					   :cljs    {:exclusions [dieter http-kit compojure selmer environ]}}
 			:plugins [[lein-midje "3.1.1"]
 					  [codox "0.6.4"]
 					  [lein-environ "0.4.0"]
@@ -36,7 +38,8 @@
 										:notify-command ["notify-send"]
 										; The standard ClojureScript compiler options:
 										; (See the ClojureScript compiler documentation for details.)
-										:compiler       {:output-to     "static/js/main.js" ; default: target/cljsbuild-main.js
+										:compiler       {:output-to     "resources/public/main.js"
 														 :optimizations :whitespace
 														 :pretty-print  true}}]}
+
 			)
---
+++ src-cljs/chaperone/ng/core.cljs
@@ -0,0 +1,7 @@
+(ns ^{:doc "The angularJS core implementation for the front end of this site"}
+	chaperone.ng.core
+	)
+
+(defn hello
+	[]
+	(.log js/console "Hello! v2"))
