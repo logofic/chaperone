b0bef37cae4742a232aeaca8b937d8714195fb90
Mark Mandel <mark.mandel@gmail.com>
2013-11-30 11:59:37 +1100

    Tmux init script for setting up all running services we need to dev on chaperone.

---
+++ tmux_init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Sets up a tmux session with a headless repl,
+# cljsbuild auto runnning and a karma instance
+# running as well.
+
+SESSION=chaperone
+
+tmux new-session -d -s $SESSION
+tmux send-keys "karma start" C-m
+tmux split-window -h
+tmux select-pane -t 1
+tmux send-keys "lein repl :headless" C-m
+tmux split-window -v -p 80
+tmux send-keys "lein with-profile cljs cljsbuild auto" C-m
+tmux attach-session -t $SESSION
