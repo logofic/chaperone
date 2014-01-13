86fb0aa20095351c9ba55718c51a33347aa99c1a
Mark Mandel <mark.mandel@gmail.com>
2014-01-13 15:00:41 +1100

    Alter my tmux init script to handle when tmux is already open.

--- tmux_init.sh
+++ tmux_init.sh
@@ -5,10 +5,17 @@
 
 SESSION=chaperone
 
-tmux new-session -d -s $SESSION
+# if not in tmux, then start a new one. But if you are, ignore it.
+if ! [ "$TERM" = "screen" ] && ! [ -n "$TMUX" ]; then
+	tmux new-session -d -s $SESSION
+fi
+
 tmux send-keys "vagrant up" C-m
 tmux send-keys "karma start" C-m
 tmux split-window -h
 tmux select-pane -t 1
 tmux send-keys "lein with-profile cljs cljsbuild auto" C-m
-tmux attach-session -t $SESSION
+
+if ! [ "$TERM" = "screen" ] && ! [ -n "$TMUX" ]; then
+	tmux attach-session -t $SESSION
+fi
